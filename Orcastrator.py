7c7bfaa01e055c1d72f1cdb76da0ccdd33064e68
MorEdri <moredri31@gmail.com>
2025-01-06 00:12:45 +0200

    Updated Orcastrator.py: user sign-in and sign-up

--- Orcastrator.py
+++ Orcastrator.py
@@ -18,6 +18,18 @@ def submit_report():
     return jsonify({"message": "submit_report"}), 200
 
 
+# New User - ask sign-up request from User Manger
+@app.route("/sign-up", methods=["POST"])
+def sign_up():
+   return jsonify({"message": "User signed up successfully"}), 200
+
+
+# Existing User - ask sign-in request from User Manger
+@app.route("/sign-in", methods=["POST"])
+def sign_in():
+    return jsonify({"message": "User signed in successfully"}), 200
+
+
 # הרצת ה-Orchestrator
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
